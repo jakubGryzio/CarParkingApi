a41eb53038a2cb768f7873ffe73589f7efe34783
Jakub Gryzio <jakub.gryzio@hexagon.com>
2025-01-09 11:19:41 +0100

    Extension for builder services

---
+++ Extensions/ApplicationServiceExtensions.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace CarParkingWebApi.Extensions
+{
+    public static class ApplicationServiceExtensions
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            services.AddDbContext<ApplicationDbContext>((options) =>
+            {
+                options.UseInMemoryDatabase("In-Memory Database");
+            });
+
+            services.AddScoped<IDbContext, ApplicationDbContext>();
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateLifetime = true,
+                        ValidateIssuerSigningKey = true,
+                        ValidIssuer = "localhost",
+                        ValidAudience = "localhost",
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("this is my custom Secret key for authentication")),
+                    };
+                });
+
+            services.AddAuthorization();
+
+            return services;
+        }
+    }
+}
